14858e812176d8c3a2effe93da7e6410733cf28d
Ashley Frank <ashley.frank@ufl.edu>
2023-09-16 20:01:44 -0400

    Added brackets to error message and added comments

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static ('public'))
 // TODO: declare the GET route /api/city/:city
 app
     .route('/api/city/:city')
-    .get(async (req, res) => {
+    .get(async (req, res) => { //async function to handle timing of data collection
 // This endpoint should call getCityInfo and getJobs and return
 // the result as JSON. 
 // The returned JSON object should have two keys:
@@ -22,14 +22,19 @@ app
 // If no city info or jobs are found,
 // the endpoint should return a 404 status
 
+    //Set location equal to the parameter from the request /:city
     var location = req.params.city
+
+    //Pull data from functions in ./util.js
     const cityInfo = await getCityInfo(location)
     const jobs =await getJobs(location)
     
+    //Once info for each is gathered, check if value exists, if not, error and quit program
     if(!(cityInfo || jobs)) 
-        return res.status(404).send("information not found")
-    const info = {cityInfo, jobs}
-    res.json(info)
+        return res.status(404).json({error: "Information not found"})
+
+    //Values exist, populate into object of keys and return in JSON format
+    res.json({cityInfo, jobs})
 
 })
  
